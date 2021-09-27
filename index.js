1005dc8fe6ca9ec89fc5d464e5035522d4915c4e
mirokiro5 <mirokiro@mail.ru>
2021-09-27 23:26:04 +0300

    added other files

---
+++ index.js
@@ -0,0 +1,46 @@
+const config = require('config.json');
+const VkBot = require('node-vk-bot-api');
+const fs = require("fs");
+const bot = new VkBot(config.token);
+const msgcount=require("./msgcount.json");
+
+bot.command('/count', (ctx) => {
+  if([2,3,4].indexOf(msgcount.count%10)+1){
+    ctx.reply(`${msgcount.count} раза`)
+  }
+  else
+  ctx.reply(`${msgcount.count} раз`)
+});
+bot.event("message_new", (ctx) => {
+  if(ctx.message.from_id==236109873){
+    msg=ctx.message.text;
+    if(msg.includes('🗿')){
+      msgcount.count++;
+      fs.writeFileSync("msgcount.json", JSON.stringify(msgcount));
+      switch(msgcount.count){
+        case '50':
+      ctx.reply(`пфф, всего 50 раз`);
+      break;
+      case '100':
+      ctx.reply(`соточка)))`);
+      break;
+      case '150':
+      ctx.reply(`Вот это уже много, 150 раз`);
+      break;
+      case '200':
+        ctx.reply(`Никита, остановись🗿, 200 раз блин`);
+        break;
+        case '1000':
+        ctx.reply(`doom guy🗿`);
+        break;
+      }
+    }
+  }
+  });
+
+ bot.startPolling((err) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log('Bot started.');
+});
